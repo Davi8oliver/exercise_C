93f02b96de879960d644131513d64c0ac0c7c211
Davi Oliveira Lopes <127767681+Davi8oliver@users.noreply.github.com>
2026-09-16 12:06:08 -0300

    Update SomaMatriz.cpp

--- SomaMatriz.cpp
+++ SomaMatriz.cpp
@@ -10,8 +10,9 @@
 
 #include <stdio.h>
 
-//Prototi·Á„o
+//Prototi√°√ß√£o
 void SomarMatrizes (int [][3], int [][3]);
+void ImprimirMatrizes (int [][3], int [][3], int [][3]);
 
 main ()
 {
@@ -23,7 +24,7 @@ main ()
 	
 }//fim do main
 
-//funÁ„o
+//fun√ß√£o
 void SomarMatrizes (int a[][3], int b[][3]){
 	
 	int matSom [3][3];
@@ -36,14 +37,44 @@ void SomarMatrizes (int a[][3], int b[][3]){
 			}
 
 	}
+
+	ImprimirMatrizes (a, b, matSom);
+
+}
+
+void ImprimirMatrizes (int a [][3], int b [][3], int soma [][3]){
+	
+	printf("\n\nConteudo da Matriz a: \n\n");
 	
 	for (int i = 0; i < 3; i++){
 			for (int j = 0; j < 3; j++){
 				
-				printf("|%d|\t", matSom[i][j]);
+				printf("|%d|\t", a[i][j]);
 			
 			}
 		puts("\n");
 	}
-
+	
+	printf("\n\nConteudo da Matriz b: \n\n");
+	
+	for (int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++){
+				
+				printf("|%d|\t", b[i][j]);
+			
+			}
+		puts("\n");
+	}
+	
+	printf("\n\nConteudo da Soma: \n\n");
+	
+	for (int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++){
+				
+				printf("|%d|\t", soma[i][j]);
+			
+			}
+		puts("\n");
+	}	
+	
 }
